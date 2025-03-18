1b4a8104f39b692776a09b711bdab69c484753a8
Victor.machado <victorviniciusmachado5@gmail.com>
2025-03-18 20:24:20 -0300

    ultimas atualizações

--- wdio.conf.js
+++ wdio.conf.js
@@ -4,8 +4,8 @@ export const config = {
    //  host: '127.0.0.1',
     // port: 4723,
  
-     user: 'oauth-victorviniciusmachado5-93739',
-     key: 'f27db06b-f6cf-4831-b591-293a792ae106',
+     user: 'oauth-arq.felipefer-fd93f',
+     key: '2a97dd57-1a60-4361-9907-6a28a47ae227',
      hostname: 'ondemand.us-west-1.saucelabs.com',
      port: 443,
      baseUrl: 'wd/hub',
@@ -28,14 +28,14 @@ export const config = {
      }*/
          {
              platformName: 'Android',
-             'appium:app': 'storage:filename=ebacshop (2).aab', // The filename of the mobile app
+             'appium:app': 'storage:filename=ebacshop (3).aab', // The filename of the mobile app
              'appium:deviceName': 'Android GoogleAPI Emulator',
              'appium:platformVersion': '12.0',
              'appium:automationName': 'UiAutomator2',
              'appium:disableIdLocatorAutocompletion': true,
              'sauce:options': {
                 build: 'appium-build-test-ebacshop17',
-                name: '<EbacShopTeste>',
+                name: '<EbacShopTeste17>',
                 deviceOrientation: 'PORTRAIT',
               },
          }
